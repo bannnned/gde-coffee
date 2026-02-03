c6d3b131f017b858d979d186a633ed647751482b
BANNED <maximjaks@gmail.com>
2026-02-03 19:36:25 +0300

    change cafes api front

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080'
+const baseURL = import.meta.env.VITE_API_BASE_URL ?? ''
 
 type NormalizedApiError = {
   status?: number
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
     host: "0.0.0.0", // Открыть на всех интерфейсах
     port: 3001,
   },
